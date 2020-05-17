35123a55e00659c4ed4ce19674f9f5a68d57d1bf
gss000 <gsp401@qq.com>
2020-05-17 16:02:21 +0800

    简化文件入口口

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+import Hello from './pages/Hello/Hello';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+	<div className="App">
+		<BrowserRouter>
+			<Switch>
+				<Route path="/hello" component={Hello} />
+			</Switch>
+		</BrowserRouter>
+	</div>,
+	document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
