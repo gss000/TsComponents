814c4f2072e0a5d0ec46cdc4ceb0ffd2ef5e0063
gss000 <gsp401@qq.com>
2020-06-10 23:34:57 +0800

    新增tooltip组件文件

--- src/pages/components/index.js
+++ src/pages/components/index.js
@@ -4,4 +4,5 @@ export { default as Switch } from './Switch';
 export { default as Rate } from './Rate/Rate';
 export { default as AuthCode } from './AuthCode';
 export { default as Popover } from './Popover/Popover';
+export { default as ToolTip } from './ToolTip/ToolTip';
 export { default as PopConfirm } from './PopConfirm/PopConfirm';
